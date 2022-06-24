40053c422ffa36242fcab7955e5c3358555f8e42
zuazo99 <gzuazo63@gmail.com>
2022-06-24 12:44:32 +0200

    Anotacion @RequestParam

---
+++ src/main/java/com/bolsadeideas/springboot/app/springboot/web/app/controllers/EjemploParamsController.java
@@ -0,0 +1,24 @@
+package com.bolsadeideas.springboot.app.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/params")
+public class EjemploParamsController {
+
+    @GetMapping("/")
+    public String index(){
+        return "params/index";
+    }
+
+
+    @GetMapping("/string")
+    public String param(@RequestParam(name = "texto", required = false, defaultValue = "algún valor") String texto,  Model model){
+        model.addAttribute("resultado", "El texto enviado es : " +texto);
+        return "params/ver";
+    }
+}
